0b3d3584334d584e3d15bf74b3634574e3879a8d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-11-28 18:16:05 +0200

    Update mySQL_queries.sql

--- src/mySQL_queries.sql
+++ src/mySQL_queries.sql
@@ -59,3 +59,24 @@ DROP PROCEDURE `getEmployeeByPhone`; CREATE DEFINER=`test`@`%` PROCEDURE `getEmp
 DROP FUNCTION `checkStationStatus`; CREATE DEFINER=`test`@`%` FUNCTION `checkStationStatus`(`stationNum` INT(11)) RETURNS INT(11) DETERMINISTIC READS SQL DATA SQL SECURITY DEFINER BEGIN DECLARE cnt int(11); SET cnt = 0; select count(*) into cnt from stations where stationNumber = stationNum; return cnt; END
 
 DROP FUNCTION `checkStationStatus`; CREATE DEFINER=`test`@`%` FUNCTION `checkStationStatus`(`stationNum` INT(11)) RETURNS INT(11) DETERMINISTIC READS SQL DATA SQL SECURITY DEFINER BEGIN DECLARE cnt int(11); DECLARE _in time; DECLARE _out time; SET cnt = 0; select count(*) into cnt from stations where stationNumber = stationNum; if cnt = 0 then return 0; end if; select `in`, `out` into _in, _out from reports where stationNumber = stationNum and date = DATE( NOW() ); return cnt; END
+
+
+DELIMITER $$
+CREATE DEFINER=`test`@`%` PROCEDURE `applyReportIn`(IN `emplId` VARCHAR(9), IN `stationNum` INT(5), IN `timeReportred` TIME, IN `dateReported` DATE)
+    NO SQL
+BEGIN
+    DECLARE _status int(1);
+      
+    SELECT checkStationStatus (stationNum) into _status;
+    CASE
+    	when _status = 2 then
+    	BEGIN
+        	insert INTO failed_reports (`in`, createdAt, updatedAt, `date`, employeeId, stationNumber)            VALUES(timeReportred, NOW(), NOW(), dateReported, emplId, stationNum);
+        END;
+    	when _status = 1 THEN
+        	BEGIN
+                      	insert INTO reports (`in`, createdAt, updatedAt, `date`, employeeId, stationNumber)            VALUES(timeReportred, NOW(), NOW(), dateReported, emplId, stationNum);
+			END;
+     END CASE;
+END$$
+DELIMITER ;
